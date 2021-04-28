e82df15c58ab8391f77626f5c2272036a30fbb0d
courage331 <courage331@gmail.com>
2021-04-29 01:03:30 +0900

    final 10

--- src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().headers().frameOptions().disable().and()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/images/**", "/js/**" , "/h2-console/**").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**" , "/h2-console/**","/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
                 .and().logout().logoutSuccessUrl("/")
